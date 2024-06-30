e8a8cebb1bdb1095d03089a9c66981aea29469bf
Muhammad Kaif Nazeer <122388541+MuhammadKaifNazeer@users.noreply.github.com>
2024-06-30 13:38:48 +0000

    completed clients section

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import RecentProjects from "@/components/RecentProjects";
 import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import Clients from "@/components/Clients";
+// import Approach from "@/components/Approach";
 
 export default function Home() {
   return (
@@ -14,7 +16,9 @@ export default function Home() {
         <Hero />
         <Grid />
         <RecentProjects />
+        <Clients />
         <Experience />
+        {/* <Approach /> */}
         <Footer />
       </div>
     </main>
