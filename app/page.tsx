daa50312b0aba05f128e20f13c7b195c65aadc9e
Muhammad Kaif Nazeer <122388541+MuhammadKaifNazeer@users.noreply.github.com>
2024-06-30 07:53:09 +0000

    completed navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
+import { navItems } from "@/data";
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import RecentProjects from "@/components/RecentProjects";
 import Footer from "@/components/Footer";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        {/* <FloatingNav /> */}
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
